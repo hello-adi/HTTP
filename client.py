bb04e589eb6b31263fa97942ccf880bb4f66c1b2
Aditya Lakshminarayanan <aditya.net09@gmail.com>
2021-10-28 18:38:52 -0700

    can accept hostname from commandline arguments

--- client.py
+++ client.py
@@ -1,16 +1,23 @@
 #imports
+#Sys to read command line arguments
 from socket import *
+import sys
+
+#The commandline arguments would be formatted as:
+#client.py server_host server_port filename
+
 
 #Host server name and port number
-serverName = '10.30.86.26'
-serverPort = 1435
+serverName = sys.argv[1]
+serverPort = sys.argv[2]
+filename = sys.argv[3]+".html"
 
 #Preparing client socket
 clientSocket = socket(AF_INET, SOCK_STREAM)
 
 #Connecting to the server and sending a GET request to obtain data.
 clientSocket.connect((serverName, serverPort))
-outputdata = 'GET /index.html HTTP/1.1\r\nHost: 10.30.86.26\r\n\r\n'
+outputdata = f'GET /{filename} HTTP/1.1\r\nHost: {serverName}\r\n\r\n'
 clientSocket.send(outputdata.encode())
 
 #While data is true, print the information recieved 
