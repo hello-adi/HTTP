fb85b056fbb193e9647194d37f94542462ca66f9
Aditya Lakshminarayanan <aditya.net09@gmail.com>
2021-10-28 17:50:48 -0700

    cleaned and formatted client.py

--- client.py
+++ client.py
@@ -1,13 +1,23 @@
+#imports
 from socket import *
 
-serverName = '10.6.70.200'
+#Host server name and port number
+serverName = '10.30.86.26'
 serverPort = 1435
+
+#Preparing client socket
 clientSocket = socket(AF_INET, SOCK_STREAM)
+
+#Connecting to the server and sending a GET request to obtain data.
 clientSocket.connect((serverName, serverPort))
-outputdata = 'GET /index.html HTTP/1.1\r\nHost: 10.6.70.200\r\n\r\n'
+outputdata = 'GET /index.html HTTP/1.1\r\nHost: 10.30.86.26\r\n\r\n'
 clientSocket.send(outputdata.encode())
+
+#While data is true, print the information recieved 
 data = 1
 while data:
     data = clientSocket.recv(1024)
     print(data.decode(), end = '')
+
+#Close socket after information is printed
 clientSocket.close()
